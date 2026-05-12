b7d88a69efee3f58ea27e36bea5006b39d70c9f5
shizuku86 <shizuku86710@gmail.com>
2026-05-13 08:32:45 +0900

    build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
+import { SemVer } from '@kairo-js/properties';
 import { TSchema, Static } from '@sinclair/typebox';
 
 interface Random {
@@ -8,6 +9,15 @@ declare class SeedRandom implements Random {
     private readonly rng;
     constructor(seed?: string);
     next(): number;
+}
+
+declare class SemVerUtils {
+    static format(version: SemVer): string;
+    static compare(a: SemVer, b: SemVer): number;
+    static rcompare(a: SemVer, b: SemVer): number;
+    static satisfies(version: SemVer, range: string): boolean;
+    static equals(a: SemVer, b: SemVer): boolean;
+    static isPrerelease(version: SemVer): boolean;
 }
 
 declare function safeJsonParse(message: string, error: () => Error): unknown;
@@ -22,4 +32,4 @@ declare function compile<TSchemaType extends TSchema>(schema: TSchemaType): Vali
 
 declare function validateTimestamp(currentTick: number, timestamp: number, timeout: number, onTimeout: () => Error, onFuture: () => Error): void;
 
-export { type Random, SeedRandom, type ValidateFunction, compile, safeJsonParse, toError, validateTimestamp };
+export { type Random, SeedRandom, SemVerUtils, type ValidateFunction, compile, safeJsonParse, toError, validateTimestamp };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export type { Random } from "./types/Random";
 export { SeedRandom } from "./utils/SeedRandom";
 
+export { SemVerUtils } from "./utils/SemVerUtils";
+
 export { safeJsonParse } from "./utils/jsonParse";
 export { toError } from "./utils/toError";
 export { compile } from "./utils/typeCompile";
